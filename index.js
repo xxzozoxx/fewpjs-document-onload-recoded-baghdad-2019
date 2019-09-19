366ef128a3c1e9adf380c20f128094dd26708de3
zozo <zahraa.jalaa99@gmail.com>
2019-09-19 21:19:41 +0300

    solved

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
+    console.log("dom is loadded");
     document.getElementById('text').innerHTML = "this is really cool!";
 });
+``
